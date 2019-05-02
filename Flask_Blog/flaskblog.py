a1bfb74577f644ba06c3348525030312528a88a0
tkalani <tanmay.k16@iiits.in>
2019-05-02 19:36:00 +0530

    Login Signup Templater

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bedff0b16a571d2ef16ccbd6e08eb7fc'
 
 posts = [
     {
@@ -24,6 +27,26 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for  {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'tk@tk.in' and form.password.data == 'tk':
+            flash(f'Logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Logged Failed', 'danger')
+            # return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)     # RUN WITHOUT EXPORTING ENV VARIABLES 
